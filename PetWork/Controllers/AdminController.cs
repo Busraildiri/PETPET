f49af0f363bb6ef3d282ed98d05b71bc2514df6c
Busraildiri <busraildiri1@gmail.com>
2026-09-04 16:25:24 +0300

    Admin paneline içerik silme yönetimi ekle

--- PetWork/Controllers/AdminController.cs
+++ PetWork/Controllers/AdminController.cs
@@ -1,159 +1,188 @@
-using System;
-using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PetWork.Data;
-using PetWork.Models;
-using Microsoft.AspNetCore.Http;
 
-namespace PetWork.Controllers
+namespace PetWork.Controllers;
+
+public class AdminController : Controller
 {
-    public class AdminController : Controller
+    private readonly PetWorkDbContext _context;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public AdminController(PetWorkDbContext context, IHttpContextAccessor httpContextAccessor)
     {
-        private readonly PetWorkDbContext _context;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        _context = context;
+        _httpContextAccessor = httpContextAccessor;
+    }
 
-        public AdminController(PetWorkDbContext context, IHttpContextAccessor httpContextAccessor)
-        {
-            _context = context;
-            _httpContextAccessor = httpContextAccessor;
-        }
+    private bool IsAdmin()
+    {
+        var userId = _httpContextAccessor.HttpContext?.Session.GetInt32("UserId");
+        return userId.HasValue && _context.Users.Any(user => user.Id == userId.Value && user.IsAdmin);
+    }
 
-        // Admin olmayan kullanıcılar için güvenlik kontrolü
-        private bool IsAdmin()
+    public IActionResult Index()
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        ViewBag.UsersCount = _context.Users.Count();
+        ViewBag.RecipesCount = _context.Recipes.Count();
+        ViewBag.DiseasesCount = _context.Diseases.Count();
+        ViewBag.GuidesCount = _context.Guides.Count();
+        ViewBag.QuestionsCount = _context.Questions.Count();
+        ViewBag.BlogsCount = _context.BlogPosts.Count();
+
+        return View();
+    }
+
+    public IActionResult Users()
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        ViewBag.CurrentUserId = _httpContextAccessor.HttpContext?.Session.GetInt32("UserId");
+        return View(_context.Users.OrderBy(user => user.Username).ToList());
+    }
+
+    public IActionResult Recipes()
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        return View(_context.Recipes.Include(recipe => recipe.User).OrderByDescending(recipe => recipe.PublishDate).ToList());
+    }
+
+    public IActionResult Diseases()
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        return View(_context.Diseases.OrderBy(disease => disease.Name).ToList());
+    }
+
+    public IActionResult Guides()
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        return View(_context.Guides.Include(guide => guide.User).OrderByDescending(guide => guide.PublishDate).ToList());
+    }
+
+    public IActionResult Questions()
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        return View(_context.Questions.Include(question => question.User).OrderByDescending(question => question.CreatedDate).ToList());
+    }
+
+    public IActionResult Blogs()
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        return View(_context.BlogPosts.Include(blog => blog.User).OrderByDescending(blog => blog.PublishDate).ToList());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id, string type)
+    {
+        if (!IsAdmin())
+            return RedirectToAction("Login", "Account");
+
+        var redirectAction = type switch
         {
-            var session = _httpContextAccessor.HttpContext.Session;
-            var userId = session.GetInt32("UserId");
-            
-            if (!userId.HasValue)
-                return false;
-                
-            var user = _context.Users.FirstOrDefault(u => u.Id == userId.Value);
-            return user != null && user.IsAdmin;
-        }
-        
-        // Admin paneli ana sayfası
-        public IActionResult Index()
+            "recipe" => nameof(Recipes),
+            "disease" => nameof(Diseases),
+            "guide" => nameof(Guides),
+            "question" => nameof(Questions),
+            "blog" => nameof(Blogs),
+            "user" => nameof(Users),
+            _ => nameof(Index)
+        };
+
+        try
         {
-            if (!IsAdmin())
-                return RedirectToAction("Login", "Account");
-                
-            ViewBag.UsersCount = _context.Users.Count();
-            ViewBag.RecipesCount = _context.Recipes.Count();
-            ViewBag.DiseasesCount = _context.Diseases.Count();
-            ViewBag.GuidesCount = _context.Guides.Count();
-            ViewBag.QuestionsCount = _context.Questions.Count();
-            
-            return View();
-        }
-        
-        // Kullanıcı yönetimi
-        public IActionResult Users()
-        {
-            if (!IsAdmin())
-                return RedirectToAction("Login", "Account");
-                
-            var users = _context.Users.ToList();
-            return View(users);
-        }
-        
-        // Tarif yönetimi
-        public IActionResult Recipes()
-        {
-            if (!IsAdmin())
-                return RedirectToAction("Login", "Account");
-                
-            var recipes = _context.Recipes.Include(r => r.User).ToList();
-            return View(recipes);
-        }
-        
-        // Hastalık bilgisi yönetimi
-        public IActionResult Diseases()
-        {
-            if (!IsAdmin())
-                return RedirectToAction("Login", "Account");
-                
-            var diseases = _context.Diseases.ToList();
-            return View(diseases);
-        }
-        
-        // Rehber yönetimi
-        public IActionResult Guides()
-        {
-            if (!IsAdmin())
-                return RedirectToAction("Login", "Account");
-                
-            var guides = _context.Guides.Include(g => g.User).ToList();
-            return View(guides);
-        }
-        
-        // Soru yönetimi
-        public IActionResult Questions()
-        {
-            if (!IsAdmin())
-                return RedirectToAction("Login", "Account");
-                
-            var questions = _context.Questions.Include(q => q.User).ToList();
-            return View(questions);
-        }
-        
-        // İçerik silme
-        [HttpPost]
-        public IActionResult Delete(int id, string type)
-        {
-            if (!IsAdmin())
-                return RedirectToAction("Login", "Account");
-                
             switch (type)
             {
                 case "recipe":
-                    var recipe = _context.Recipes.Find(id);
-                    if (recipe != null)
-                    {
-                        _context.Recipes.Remove(recipe);
-                        _context.SaveChanges();
-                    }
-                    return RedirectToAction("Recipes");
-                    
+                    RemoveEntity(_context.Recipes.Find(id), _context.Recipes, "Tarif silindi.");
+                    break;
                 case "disease":
-                    var disease = _context.Diseases.Find(id);
-                    if (disease != null)
-                    {
-                        _context.Diseases.Remove(disease);
-                        _context.SaveChanges();
-                    }
-                    return RedirectToAction("Diseases");
-                    
+                    RemoveEntity(_context.Diseases.Find(id), _context.Diseases, "Hastalık kaydı silindi.");
+                    break;
                 case "guide":
-                    var guide = _context.Guides.Find(id);
-                    if (guide != null)
-                    {
-                        _context.Guides.Remove(guide);
-                        _context.SaveChanges();
-                    }
-                    return RedirectToAction("Guides");
-                    
+                    RemoveEntity(_context.Guides.Find(id), _context.Guides, "Rehber silindi.");
+                    break;
                 case "question":
-                    var question = _context.Questions.Find(id);
-                    if (question != null)
-                    {
-                        _context.Questions.Remove(question);
-                        _context.SaveChanges();
-                    }
-                    return RedirectToAction("Questions");
-                    
+                    RemoveEntity(_context.Questions.Find(id), _context.Questions, "Soru ve bağlı cevapları silindi.");
+                    break;
+                case "blog":
+                    RemoveEntity(_context.BlogPosts.Find(id), _context.BlogPosts, "Blog yazısı silindi.");
+                    break;
                 case "user":
-                    var user = _context.Users.Find(id);
-                    if (user != null)
-                    {
-                        _context.Users.Remove(user);
-                        _context.SaveChanges();
-                    }
-                    return RedirectToAction("Users");
-                    
+                    DeleteUser(id);
+                    break;
                 default:
-                    return RedirectToAction("Index");
+                    TempData["ErrorMessage"] = "Geçersiz silme isteği.";
+                    break;
             }
         }
+        catch (DbUpdateException)
+        {
+            TempData["ErrorMessage"] = "Kayıt, bağlı veriler nedeniyle silinemedi.";
+        }
+
+        return RedirectToAction(redirectAction);
     }
-} 
+
+    private void RemoveEntity<TEntity>(TEntity? entity, DbSet<TEntity> entities, string successMessage)
+        where TEntity : class
+    {
+        if (entity is null)
+        {
+            TempData["ErrorMessage"] = "Silinecek kayıt bulunamadı.";
+            return;
+        }
+
+        entities.Remove(entity);
+        _context.SaveChanges();
+        TempData["SuccessMessage"] = successMessage;
+    }
+
+    private void DeleteUser(int id)
+    {
+        var currentUserId = _httpContextAccessor.HttpContext?.Session.GetInt32("UserId");
+        if (currentUserId == id)
+        {
+            TempData["ErrorMessage"] = "Oturum açtığınız admin hesabını silemezsiniz.";
+            return;
+        }
+
+        var user = _context.Users.Find(id);
+        if (user is null)
+        {
+            TempData["ErrorMessage"] = "Silinecek kullanıcı bulunamadı.";
+            return;
+        }
+
+        if (user.IsAdmin && _context.Users.Count(candidate => candidate.IsAdmin) <= 1)
+        {
+            TempData["ErrorMessage"] = "Sistemdeki son admin hesabı silinemez.";
+            return;
+        }
+
+        using var transaction = _context.Database.BeginTransaction();
+        _context.Answers.RemoveRange(_context.Answers.Where(answer => answer.UserId == id));
+        _context.Recipes.RemoveRange(_context.Recipes.Where(recipe => recipe.UserId == id));
+        _context.Questions.RemoveRange(_context.Questions.Where(question => question.UserId == id));
+        _context.Users.Remove(user);
+        _context.SaveChanges();
+        transaction.Commit();
+
+        TempData["SuccessMessage"] = "Kullanıcı ve bağlı içerikleri silindi.";
+    }
+}
